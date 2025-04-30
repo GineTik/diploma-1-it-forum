56236851db638ada97553e644ad20fbd04fbc54a
GineTik <densheva18072003@gmail.com>
2025-04-30 21:47:18 +0300

    Update next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,7 +4,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const nextConfig: NextConfig = {
-  output: "standalone",
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
